6a833d5d7dcf9a18aa55c1156fec9326b114fbdd
Juneseok Byun <byun@hbsmith.io>
2021-04-20 13:43:20 +0900

    GRA-54 Store Controller 정리

--- src/main/java/com/example/demo/api/controller/StoreController.java
+++ src/main/java/com/example/demo/api/controller/StoreController.java
@@ -1,7 +1,6 @@
 package com.example.demo.api.controller;
 
 import com.example.demo.api.entity.Store;
-import com.example.demo.api.entity.User;
 import com.example.demo.api.model.response.CommonResult;
 import com.example.demo.api.model.response.SingleResult;
 import com.example.demo.api.repo.StoreJpaRepo;
@@ -11,8 +10,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Api(tags = {"Store"})
 @RequiredArgsConstructor
@@ -24,13 +21,13 @@ public class StoreController {
     private final ResponseService responseService;
 
     @ApiOperation(value = "식당 정보 조회", notes = "이름으로 식당 정보를 조회한다")
-    @GetMapping(value = "/{storeName}")
-    public SingleResult<Store> findStoreByName(@PathVariable String storeName){
-        return responseService.getSingleResult(storeJpaRepo.findByRname(storeName));
+    @GetMapping(value = "/{r_name}")
+    public SingleResult<Store> findStoreByName(@PathVariable String r_name){
+        return responseService.getSingleResult(storeJpaRepo.findByRname(r_name));
     }
 
     @ApiOperation(value = "상점 등록", notes = "상점 등록한다.")
-    @PostMapping(value = "/make_store")
+    @PostMapping(value = "/create")
     public SingleResult<Store> make_store(@ApiParam(value = "식당 이름", required = true)@RequestParam String r_name,
             @ApiParam(value = "식당 설명", required = true)@RequestParam String r_detail,
             @ApiParam(value = "식당 영업 시간", required = true)@RequestParam String r_hours_operation,
@@ -48,7 +45,7 @@ public class StoreController {
     }
 
     @ApiOperation(value = "상점 정보 갱신", notes = "이름으로 상점 찾아서 해당 상점 정보를 갱신한다.")
-    @PutMapping(value = "/update_store")
+    @PutMapping(value = "/update")
     public SingleResult<Store> modify_store(
             @ApiParam(value = "기존 식당 이름", required = true)@RequestParam String r_name,
             @ApiParam(value = "새로운 식당 이름. 안바꿀거면 그대로.", required = true)@RequestParam String new_r_name,
@@ -69,7 +66,7 @@ public class StoreController {
     }
 
     @ApiOperation(value = "상점 해지", notes = "상점 이름으로 삭제한다.")
-    @DeleteMapping(value = "/delete_store/{r_name}")
+    @DeleteMapping(value = "/delete/{r_name}")
     public CommonResult delete_store(
             @ApiParam(value = "식당 이름", required = true)@PathVariable String r_name) {
         storeJpaRepo.deleteByRname(r_name);
