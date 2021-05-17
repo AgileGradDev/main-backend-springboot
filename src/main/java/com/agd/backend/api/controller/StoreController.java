251386807237c543bdc7b11f8139b5ad0623ec26
Juneseok Byun <byun@hbsmith.io>
2021-05-17 16:14:41 +0900

    Store Controller 약간 수정.

--- src/main/java/com/agd/backend/api/controller/StoreController.java
+++ src/main/java/com/agd/backend/api/controller/StoreController.java
@@ -6,11 +6,15 @@ import com.agd.backend.api.service.ResponseService;
 import com.agd.backend.api.entity.Store;
 import com.agd.backend.api.model.response.CommonResult;
 import com.agd.backend.api.repository.StoreRepository;
+import com.agd.backend.api.service.store.StoreService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @Api(tags = {"Store"})
 @RequiredArgsConstructor
@@ -18,19 +22,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/store")
 public class StoreController {
 
-    private final StoreRepository storeJpaRepository;
+    private final StoreRepository storeRepository;
+    private final StoreService storeService;
     private final ResponseService responseService;
 
-    @ApiOperation(value = "식당 정보 조회", notes = "이름으로 식당 정보를 조회한다")
-    @GetMapping(value = "/{name}")
-    public SingleResult<Store> findStoreByName(@PathVariable String name) {
-        return responseService.getSingleResult(storeJpaRepository.findByName(name));
+    @GetMapping(value = "/{id}")
+    public Store retrieve(@PathVariable UUID id) {
+        return storeService.retrieveStore(id);
     }
 
     @ApiOperation(value = "MVP 전용 API", notes = "식당 정보 전체 조회")
     @GetMapping(value = "/all")
-    public ListResult<Store> listStore() {
-        return responseService.getListResult(storeJpaRepository.findAll());
+    public List<Store> listStore() {
+        return storeService.ListStore();
     }
 
     @ApiOperation(value = "상점 등록", notes = "상점 등록한다.")
@@ -48,7 +52,7 @@ public class StoreController {
                 .rating(rating)
                 .location(location)
                 .build();
-        return responseService.getSingleResult(storeJpaRepository.save(store));
+        return responseService.getSingleResult(storeRepository.save(store));
     }
 
     @ApiOperation(value = "상점 정보 갱신", notes = "이름으로 상점 찾아서 해당 상점 정보를 갱신한다.")
@@ -60,8 +64,8 @@ public class StoreController {
             @ApiParam(value = "식당 영업 시간", required = true) @RequestParam String hours_operation,
             @ApiParam(value = "식당 평점", required = true) @RequestParam float rating,
             @ApiParam(value = "식당 위치", required = true) @RequestParam String location) {
-        Store previous = storeJpaRepository.findByName(name);
-        storeJpaRepository.deleteById(previous.getId());
+        Store previous = storeRepository.findByName(name);
+        storeRepository.deleteById(previous.getId());
         Store store = Store.builder()
                 .name(new_name)
                 .description(detail)
@@ -69,14 +73,14 @@ public class StoreController {
                 .rating(rating)
                 .location(location)
                 .build();
-        return responseService.getSingleResult(storeJpaRepository.save(store));
+        return responseService.getSingleResult(storeRepository.save(store));
     }
 
     @ApiOperation(value = "상점 해지", notes = "상점 이름으로 삭제한다.")
     @DeleteMapping(value = "/delete/{name}")
     public CommonResult delete_store(
             @ApiParam(value = "식당 이름", required = true) @PathVariable String name) {
-        storeJpaRepository.deleteByName(name);
+        storeRepository.deleteByName(name);
         return responseService.getSuccessResult();
     }
 }
--- src/main/java/com/agd/backend/api/service/store/StoreService.java
+++ src/main/java/com/agd/backend/api/service/store/StoreService.java
@@ -1,10 +1,13 @@
 package com.agd.backend.api.service.store;
 
+import com.agd.backend.api.entity.Store;
 import com.agd.backend.api.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -13,4 +16,11 @@ public class StoreService {
 
     private final StoreRepository storeRepository;
 
+    public Store retrieveStore(UUID id) {
+        return storeRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public List<Store> ListStore() {
+        return storeRepository.findAll();
+    }
 }
